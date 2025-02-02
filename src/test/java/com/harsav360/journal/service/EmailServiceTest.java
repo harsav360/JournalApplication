777176d709c6ef4b017112ae43e8141695d1492c
harsav360 <annu7418@gmail.com>
2025-02-02 10:25:34 +0530

    Java Mail sender implemented

---
+++ src/test/java/com/harsav360/journal/service/EmailServiceTest.java
@@ -0,0 +1,18 @@
+package com.harsav360.journal.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailServiceTest {
+
+    @Autowired
+    private EmailService emailService;
+
+//    @Test
+//    void testSendMail() {
+//        emailService.sendMail("gdgdg@gmail.com","Testing the Java Mail sender","Hello Pardhan");
+//
+//    }
+}
