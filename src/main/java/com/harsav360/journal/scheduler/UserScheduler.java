1aeaf9736fe3bc104523fab6c10aabe25be3c894
harsav360 <annu7418@gmail.com>
2025-02-03 23:18:21 +0530

    Swagger API added

--- src/main/java/com/harsav360/journal/scheduler/UserScheduler.java
+++ src/main/java/com/harsav360/journal/scheduler/UserScheduler.java
@@ -62,7 +62,7 @@ public class UserScheduler {
             if (mostFrequentSentiment != null) {
                 SentimentData sentimentData = SentimentData.builder().email(user.getEmail()).sentiment("Sentiment for last 7 days " + mostFrequentSentiment).build();
                 try{
-                    kafkaTemplate.send("weekly-sentiments", sentimentData.getEmail(), sentimentData);
+                    kafkaTemplate.send("weekly_sentiments", sentimentData.getEmail(), sentimentData);
                 }catch (Exception e){
                     emailService.sendMail(sentimentData.getEmail(), "Sentiment for previous week", sentimentData.getSentiment());
                 }
--- src/main/java/com/harsav360/journal/service/SentimentConsumerService.java
+++ src/main/java/com/harsav360/journal/service/SentimentConsumerService.java
@@ -4,8 +4,6 @@ package com.harsav360.journal.service;
 import com.harsav360.journal.model.SentimentData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,7 +12,7 @@ public class SentimentConsumerService {
     @Autowired
     private EmailService emailService;
 
-    @KafkaListener(topics = "weekly-sentiments", groupId = "weekly-sentiment-group")
+    @KafkaListener(topics = "weekly_sentiments", groupId = "weekly-sentiment-group")
     public void consume(SentimentData sentimentData) {
         sendEmail(sentimentData);
     }
